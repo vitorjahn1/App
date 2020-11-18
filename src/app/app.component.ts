83c3bdf9c7ce66ec9bdc043e76c73ae77c8f2b8b
vitor <vitorjosejahn@hotmail.com>
2020-11-18 18:52:38 -0300

    Teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'App';
+  readonly Searsh_URL = 'http://localhost:64171'
+  result : Observable<any>;
+  buscar:any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.buscar = {}
+   
+
+  }
+  busca(){
+
+    this.result = this.http.get(this.Searsh_URL);
+   
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    FormsModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
